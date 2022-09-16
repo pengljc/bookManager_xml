83a68b0776ffcaba1c167bc6d8b57385e49cbe1d
sevenchilde <2378190715@qq.com>
2022-09-16 09:29:33 +0800

    hot-fix BookManager_xml first commit  0.0.3.1 create class Hello from package java

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Hello
+ * @Description TODO
+ * @Author liujianpeng
+ * @Date 2022/9/16
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+    }
+}
